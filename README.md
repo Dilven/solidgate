2358f3ea5f3882d13929021ec6a40951a08d78b3
Dilven <mateusiakkrystian@gmail.com>
2021-11-26 23:51:51 +0100

    init project

---
+++ README.md
@@ -0,0 +1,20 @@
+## About
+Boilerplate for quick check of ideas
+
+Includes
+- react
+- next
+- hapi/boom
+- react-query
+- axios
+- zod
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { defaultOptions } from '@/config/cache'
+
+const queryClient = new QueryClient({ defaultOptions });
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
+}
+
+export default MyApp
