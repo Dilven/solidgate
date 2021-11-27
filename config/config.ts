9a17318de42509de1db96eef593255fa038ec05a
Dilven <mateusiakkrystian@gmail.com>
2021-11-27 02:07:46 +0100

    add next-seo

--- config/config.ts
+++ config/config.ts
@@ -1,7 +1,7 @@
 import getConfigNext from "next/config";
 
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfigNext();
-type EnvVariable = "NEXT_PUBLIC_INTERNAL_API_TIMEOUT";
+type EnvVariable = "NEXT_PUBLIC_URL";
 
 export const getConfig = (name: EnvVariable) => {
   let val: string | undefined;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,44 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { defaultOptions } from "@/config/cache";
 import { Layout } from "@/components/Layout";
 import Head from "next/head";
+import { DefaultSeo } from "next-seo";
+import { getConfig } from "@/config/config";
 
 const queryClient = new QueryClient({ defaultOptions });
+
+const meta = {
+  title: "Solidgate",
+  description: `Solidgate to firma zajmująca się wykonywanie ogrodzeń, bram, furtek, balustrad schodowych i innych elementów kutych!`,
+};
+export const titleTemplate = `%s | ${meta.title}`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <DefaultSeo
+        title={meta.title}
+        description={meta.description}
+        openGraph={{
+          type: "website",
+          title: meta.title,
+          locale: "pl_PL",
+          url: `https://${getConfig("NEXT_PUBLIC_URL")}${asPath}`,
+          description: meta.description,
+          images: [
+            {
+              url: `https://${getConfig("NEXT_PUBLIC_URL")}/logo.png`,
+              width: 1000,
+              height: 1000,
+            },
+          ],
+          site_name: meta.title,
+        }}
+      />
       <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta
+          name="viewport"
+          content="width=device-width, user-scalable=yes, initial-scale=1.0, viewport-fit=cover"
+        />
       </Head>
       <Layout>
         <Component {...pageProps} />
