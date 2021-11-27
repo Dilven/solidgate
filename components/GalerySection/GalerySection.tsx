0c7fd78afc71262ac9d78da648e9842d0c81f1da
Dilven <mateusiakkrystian@gmail.com>
2021-11-27 17:03:26 +0100

    Add basic separator and galery section

---
+++ components/GalerySection/GalerySection.tsx
@@ -0,0 +1,9 @@
+import styles from './GalerySection.module.scss';
+
+export const GalerySection = () => {
+  return (
+    <section className={styles.section}>
+        galery section
+    </section>
+  );
+};
---
+++ components/Separator/Separator.tsx
@@ -0,0 +1,7 @@
+
+import * as BasicSeparator from '@radix-ui/react-separator';
+import styles from './Separator.module.scss';
+
+export const Separator = () => (
+    <BasicSeparator.Root className={styles.separator} />
+)
