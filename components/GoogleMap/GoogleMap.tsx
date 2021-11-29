6b318afdbaeb7f5beb0b0c6c697031b2cadd37c6
Dilven <mateusiakkrystian@gmail.com>
2021-11-29 13:59:43 +0100

    prepare query for google map

--- components/GoogleMap/GoogleMap.tsx
+++ components/GoogleMap/GoogleMap.tsx
@@ -1,4 +1,14 @@
 import { getConfig } from "@/config/config";
+import { getQueryParams } from "@/helpers/get-query-params";
+
+const query = getQueryParams({
+  q: "ChIJDb1D_KGDEEcRQT-VsTN2P",
+  key: getConfig("NEXT_PUBLIC_GOOGLE_MAPS_API_KEY"),
+  center: "51.144349,18.6132",
+  zoom: 16,
+});
+const endpoint = "https://www.google.com/maps/embed/v1/place";
+const src = `${endpoint}${query}`;
 
 export const GoogleMap = () => (
   <iframe
@@ -6,11 +16,7 @@ export const GoogleMap = () => (
     height="400"
     loading="lazy"
     frameBorder="0"
-    src={`https://www.google.com/maps/embed/v1/place?key=${getConfig('NEXT_PUBLIC_GOOGLE_MAPS_API_KEY')}
-          &amp;q=ChIJDb1D_KGDEEcRQT-VsTN2P8w
-          &amp;center=51.144349,18.6132
-          &amp;zoom=16`}
+    src={src}
     allowFullScreen
   ></iframe>
 );
-// style="width: 100%; border:0;"
