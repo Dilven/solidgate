715acce03b2259529dba418ca21d1eabcc38680a
kamilmateusiak <rezyer@gmail.com>
2021-12-11 00:36:53 +0100

    feat(Photo): adding more images to balustrades page, creating fences gallery

--- pages/galeria/ogrodzenia.tsx
+++ pages/galeria/ogrodzenia.tsx
@@ -3,11 +3,17 @@ import React from "react";
 import { Layout } from "@/components/Layout/Layout";
 import banner from "../../public/main-banner.jpg";
 import { pageTitles } from "@/helpers/metadata";
+import { Gallery } from "@/components/Gallery/Gallery";
+import { images } from "data/gallery/fences";
 
 const Page = () => {
   return (
     <Layout title={pageTitles.ogrodzenia} banner={banner}>
-      Ogrodzenia
+      <p>W naszej ofercie znajdą państwo solidne i świetnie wykonane ogrodzenia. Mają państwo gwarancje satysfakcji na długie lata poprzez zabezpieczenie naszych produktów przed wpływem warunków atmosferycznych. Każde nasze ogrodzenie transportowane jest do cynkowni w miejscowości Wieluń i poddawane cynkowaniu ogniowemu po czym zostaje jeszcze lakierowane proszkowo. Dzięki tym procesom otrzymujemy pięknie wykonany produkt, który będzie nas cieszył przez długi czas. Dobrze wybrane ogrodzenie wraz z bramą i furtką tworzą piękną wizytówkę naszego domu.</p>
+      <p>Ogrodzenia wykonujemy z uprzednio przygotowanych elementów kutych przygotowanych w wielu ciekawych kształtach. Dzięki temu mogą Państwo w łatwy sposób skomponować swoje własne, oryginalne ogrodzenie.</p>
+      <p>Na specjalne zamówienie wykonujemy nie tylko ogrodzenia stalowe, ale także zawierające aluminiowe elementy, które charakteryzują się mniejszym ciężarem i brakiem konieczności cynkowania.</p>
+      <p>Oczywiście tak jak dla naszych innych produktów: balustrady, balustrady schodowe, furtki i bramy kute, ogrodzenia montujemy w Wileuniu i okolicach, ale także wyjazdy do 200km nie stanowią dla nas problemu.</p>
+      <Gallery images={images} />
     </Layout>
   );
 };
